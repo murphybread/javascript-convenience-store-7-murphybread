c4d047c8a1d5da48cdf4abeeb15f0f7d29b09108
Murphybread <nargene@gmail.com>
2024-11-07 16:46:49 +0900

    fix(Stock): rename quantity and promotion

--- src/inventory/Stock.js
+++ src/inventory/Stock.js
@@ -1,22 +1,22 @@
 class Stock {
   #name;
   #price;
-  #count;
-  #promotionType;
+  #quantity;
+  #promotion;
 
-  constructor(name, price, count, promotionType) {
+  constructor(name, price, quantity, promotion) {
     this.#name = name;
     this.#price = price;
-    this.#count = count;
-    this.#promotionType = promotionType;
+    this.#quantity = quantity;
+    this.#promotion = promotion;
   }
 
   getDetails() {
     return {
       name: this.#name,
       price: this.#price,
-      count: this.#count,
-      promotionType: this.#promotionType,
+      quantity: this.#quantity,
+      promotion: this.#promotion,
     };
   }
 }
