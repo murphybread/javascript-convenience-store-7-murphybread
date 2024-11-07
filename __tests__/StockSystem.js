6fdfc07f0d1ba30b38752d85d8f83a8de3c47c02
Murphybread <nargene@gmail.com>
2024-11-07 16:07:04 +0900

    refactor(StockSystme): change stockSystem value to stockList

--- __tests__/StockSystem.js
+++ __tests__/StockSystem.js
@@ -1,4 +1,4 @@
-const stockSystem = [
+const stockList = [
   {
     name: "goods1",
     price: 1000,
@@ -7,11 +7,11 @@ const stockSystem = [
   },
 ];
 
-describe("StockSystem 클래스", () => {
+describe("stockList 클래스", () => {
   test("재고 수량 고려한 결제 가능 여부 반환: 재고량이 입력받은 숫자보다 큰 경우 true반환", () => {
     const userInput = ["goods1", 5];
 
-    const result = stockSystem.some((stock) => {
+    const result = stockList.some((stock) => {
       if (stock.name === userInput[0]) {
         return stock.count >= userInput[1];
       }
@@ -23,7 +23,7 @@ describe("StockSystem 클래스", () => {
   test("재고 수량 고려한 결제 가능 여부 반환: 재고량이 입력받은 숫자보다 작은 경우 false반환", () => {
     const userInput = ["goods1", 50];
 
-    const result = stockSystem.some((stock) => {
+    const result = stockList.some((stock) => {
       if (stock.name === userInput[0]) {
         return stock.count > userInput[1];
       }
@@ -33,7 +33,7 @@ describe("StockSystem 클래스", () => {
   test("고객이 상품을 구매할 때마다, 결제된 수량만큼 해당 상품의 재고에서 차감하여 수량을 관리한다.", () => {
     const userInput = ["goods1", 3];
     const orderCount = userInput[1];
-    const updatedStock = stockSystem.map((stock) => {
+    const updatedStock = stockList.map((stock) => {
       if (stock.name === userInput[0] && stock.count >= orderCount) {
         return { ...stock, count: stock.count - orderCount };
       }
