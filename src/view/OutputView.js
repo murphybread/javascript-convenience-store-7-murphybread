e3b2491113a11a9bdbb06e6faf8ece0f1af89bfc
Murphybread <nargene@gmail.com>
2024-11-10 02:07:43 +0900

    fix(view): const to class

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,23 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import MarkdownToObjectReader from "../utils/MarkdownToObjectReader.js";
 
-const OutputView = {
+class OutputView {
   printIntroduction() {
     MissionUtils.Console.print("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
-  },
+  }
   getPromotion(promotion) {
     if (promotion !== "null") {
       return ` ${promotion}`;
     }
     return "";
-  },
+  }
 
   getStockStatus(quantity) {
     if (quantity > 0) {
       return `${quantity}개`;
     }
     return "재고 없음";
-  },
+  }
 
   printProducts() {
     const items = MarkdownToObjectReader.parseFile("products.md");
@@ -26,9 +26,10 @@ const OutputView = {
       const stockStatus = this.getStockStatus(item.quantity);
       MissionUtils.Console.print(`- ${item.name} ${item.price.toLocaleString("ko-KR")}원 ${stockStatus}${promotion}`);
     });
+    MissionUtils.Console.print("\n");
     // ...
   }
   // ...
-};
-OutputView.printIntroduction();
-OutputView.printProducts();
+}
+
+export default OutputView;
