9bd2c89f09a3c7175c5b45eb10bf2e969d8b8e16
Murphybread <nargene@gmail.com>
2024-11-10 02:08:05 +0900

    feat(view): readitem and parseInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import OuputView from "./view/OutputView.js";
+import InputView from "./view/InputView.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.outputView = new OuputView();
+    this.inputView = new InputView();
+  }
+
+  async run() {
+    this.outputView.printIntroduction();
+    this.outputView.printProducts();
+    const input = await this.inputView.readItem();
+    console.log(this.inputView.parseUserInput(input));
+  }
 }
 
 export default App;
---
+++ src/view/InputView.js
@@ -0,0 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class InputView {
+  async readItem() {
+    const input = await MissionUtils.Console.readLineAsync("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n");
+    return input;
+  }
+
+  parseUserInput(input) {
+    const items = input.split(",");
+    return items.map((item) => {
+      const cleanedItem = item.replace(/[\[\]\s]/g, "");
+      const [name, quantity] = cleanedItem.split("-");
+      return [name, Number(quantity)];
+    });
+  }
+
+  // ...
+}
+
+export default InputView;
