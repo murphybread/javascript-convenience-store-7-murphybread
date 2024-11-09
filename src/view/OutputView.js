b679576a7fe3ae8967427a75ebbb97b5ea37eeaf
Murphybread <nargene@gmail.com>
2024-11-09 23:34:00 +0900

    feat(view): display introduce and current stock list

---
+++ src/view/OutputView.js
@@ -0,0 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import MarkdownToObjectReader from "../utils/MarkdownToObjectReader.js";
+
+const OutputView = {
+  printIntroduction() {
+    MissionUtils.Console.print("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
+  },
+  getPromotion(promotion) {
+    if (promotion !== "null") {
+      return ` ${promotion}`;
+    }
+    return "";
+  },
+
+  getStockStatus(quantity) {
+    if (quantity > 0) {
+      return `${quantity}개`;
+    }
+    return "재고 없음";
+  },
+
+  printProducts() {
+    const items = MarkdownToObjectReader.parseFile("products.md");
+    items.forEach((item) => {
+      const promotion = this.getPromotion(item.promotion);
+      const stockStatus = this.getStockStatus(item.quantity);
+      MissionUtils.Console.print(`- ${item.name} ${item.price.toLocaleString("ko-KR")}원 ${stockStatus}${promotion}`);
+    });
+    // ...
+  }
+  // ...
+};
+OutputView.printIntroduction();
+OutputView.printProducts();
