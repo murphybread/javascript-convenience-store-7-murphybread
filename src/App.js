b2a702fb6833fc2bb7046cc56c656d4306e4420c
Murphybread <nargene@gmail.com>
2024-11-10 16:04:16 +0900

    feat(app): restart app stock system sets using while

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import OuputView from "./view/OutputView.js";
 import InputView from "./view/InputView.js";
 import StockSystem from "./inventory/Stocksystem.js";
 import MembershipDiscount from "./discounts/MembershipDiscount.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
@@ -11,18 +12,26 @@ class App {
   }
 
   async run() {
-    this.outputView.printIntroduction();
-    this.outputView.printProducts();
+    let keepRunning = true;
 
-    const input = await InputView.readItem();
-    const inputList = this.inputView.parseUserInput(input);
-    inputList.forEach((input) => {
-      this.stockSystem.calculateTotalPrice(input[0], input[1]);
-      this.stockSystem.testwriteUpdatedStockToFile(input[0], input[1]);
-    });
+    while (keepRunning) {
+      this.outputView.printIntroduction();
+      this.outputView.printProducts();
 
-    await InputView.requestMembershipDiscount(this.stockSystem.normalPrice);
-    await StockSystem.initializeTestMd();
+      const input = await InputView.readItem();
+      const inputList = this.inputView.parseUserInput(input);
+      inputList.forEach((input) => {
+        this.stockSystem.calculateTotalPrice(input[0], input[1]);
+        this.stockSystem.testwriteUpdatedStockToFile(input[0], input[1]);
+      });
+
+      await InputView.requestMembershipDiscount(this.stockSystem.normalPrice);
+      await StockSystem.initializeTestMd();
+
+      // 사용자에게 다시 실행할지 묻는 부분 추가
+      const continueAnswer = await MissionUtils.Console.readLineAsync("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n");
+      keepRunning = continueAnswer.toLowerCase() === "y";
+    }
   }
 }
 
