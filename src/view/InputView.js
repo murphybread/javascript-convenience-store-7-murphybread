62e29476d2124473f9a8cc572761a0e6314d3982
Murphybread <nargene@gmail.com>
2024-11-10 12:25:01 +0900

    refactor(view): change readItem to static

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class InputView {
-  async readItem() {
+  static async readItem() {
     const input = await MissionUtils.Console.readLineAsync("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n");
     return input;
   }
