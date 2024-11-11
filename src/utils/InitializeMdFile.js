8ce088cbd6f33b6dca5be960859b710217e9de06
Murphybread <nargene@gmail.com>
2024-11-11 16:03:00 +0900

    refacotr(utils): execute initialize iwthout input

--- src/utils/InitializeMdFile.js
+++ src/utils/InitializeMdFile.js
@@ -6,16 +6,13 @@ import fs from "fs";
 
 class MarkdownFileProcessor {
   static async initializeTestMd() {
-    const initializeAnwser = await MissionUtils.Console.readLineAsync("\ntest.md를 초기화 할까요?");
-    if (MEMBERSHIP_STATUS[initializeAnwser]) {
-      const items = StockSystem.parseFile("products.md");
-      const filePath = path.join(DIRECTORY_PATH, TEST_FILE);
-      let markdownContent = "name,price,quantity,promotion\n";
-      items.forEach((item) => {
-        markdownContent += `${item.name},${item.price},${item.quantity},${item.promotion}\n`;
-      });
-      fs.writeFileSync(filePath, markdownContent, "utf8");
-    }
+    const items = StockSystem.parseFile("products.md");
+    const filePath = path.join(DIRECTORY_PATH, TEST_FILE);
+    let markdownContent = "name,price,quantity,promotion\n";
+    items.forEach((item) => {
+      markdownContent += `${item.name},${item.price},${item.quantity},${item.promotion}\n`;
+    });
+    fs.writeFileSync(filePath, markdownContent, "utf8");
   }
 }
 
