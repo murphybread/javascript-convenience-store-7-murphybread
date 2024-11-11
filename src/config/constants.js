331eca92b62c457ff3214e34150b13ef4337bb59
murphybread <nargene@gmail.com>
2024-11-11 14:05:28 +0000

    fix: directory_path

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,19 +1,15 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-let __filename = "";
-let __dirname = "";
+// let __dirname;
 
-try {
-  // 'import.meta.url'을 런타임에 평가하여 구문 오류를 피합니다.
-  const metaUrl = eval("import.meta.url");
-  __filename = fileURLToPath(metaUrl);
-  __dirname = path.dirname(__filename);
-} catch (e) {
-  // CommonJS 환경에서는 기존의 __filename과 __dirname을 사용합니다.
-  __filename = typeof __filename !== "undefined" ? __filename : "";
-  __dirname = typeof __dirname !== "undefined" ? __dirname : "";
-}
+// try {
+//   const __filename = fileURLToPath(eval('import.meta.url'));
+//   __dirname = path.dirname(__filename);
+// } catch (e) {
+//   // Jest 환경에서는 CommonJS 모듈 시스템을 사용하므로 __dirname이 이미 정의되어 있습니다.
+//   __dirname = __dirname || process.cwd();
+// }
 
 export const MEMBERSHIP_STATUS = {
   Y: true,
@@ -22,5 +18,5 @@ export const MEMBERSHIP_STATUS = {
   n: false
 };
 
-export const DIRECTORY_PATH = path.join(__dirname, "../../public/");
+export const DIRECTORY_PATH = path.join(process.cwd(),'public');
 export const TEST_FILE = "products.md";
